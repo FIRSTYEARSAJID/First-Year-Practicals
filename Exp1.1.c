00617e11a22adf0d9428af2960db2b70d47b8aae
FIRSTYEARSAJID <habibunnisha897@gmail.com>
2024-11-14 12:26:10 +0530

    Update Exp1.1.c

--- Exp1.1.c
+++ Exp1.1.c
@@ -1,17 +1,17 @@
 /*Name: Sajid
 UIN:241P064
 Title:WAP to print your name,age,class,division and UIN on the screen. Use escape sequences to properly format the output. Experiment 1, task 1
-Roll Number:53 */
+Roll Number:58 */
 #include<stdio.h>
 
 
 int main()
 {
-  printf("Name\t\t:\tM Moin M Sameer Bhurani\n");
+  printf("Name\t\t:\tSajid\n");
   printf("Age\t\t\t:\t18\n");
   printf("Class\t:\tFirst Year\n");
   printf("Div\t\t\t:\tC3\n");
-  printf("UIN\t\t\t:\t241p059\n");
+  printf("UIN\t\t\t:\t241p064\n");
 
 
   return 0;
@@ -23,7 +23,7 @@ Name            :       Sajid
 Age                     :       18
 Class   :       First Year
 Div                     :       C3
-UIN                     :       241p065
+UIN                     :       241p064
 
 Process returned 0 (0x0)   execution time : 0.072 s
 Press any key to continue.*/
